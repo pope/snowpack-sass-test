d66889e6a9388bb9e645f8b8a961e2bc7050bc28
K. Adam Christensen <pope@shifteleven.com>
2021-02-01 05:20:41 +0000

    Add type to TestRunnerConfig.

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -3,6 +3,7 @@ const { puppeteerLauncher } = require('@web/test-runner-puppeteer');
 // NODE_ENV=test - Needed by "@snowpack/web-test-runner-plugin"
 process.env.NODE_ENV = 'test';
 
+/** @type {import('@web/test-runner').TestRunnerConfig} */
 module.exports = {
 	files: 'src/**/*.test.{js,ts,tsx}',
 	browsers: [
